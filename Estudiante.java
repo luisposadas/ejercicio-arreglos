384c18c76100d8db33cc38597f17f9ba7549678e
Luis Posadas <luisposadasp96@outlook.es>
2017-08-29 17:06:55 -0500

    se agregaron comentarios

--- Estudiante.java
+++ Estudiante.java
@@ -2,12 +2,27 @@ public class Estudiante
 {
     private String nombre;
     private int clave;
-    public Estudiante(int claveIni, String nombreIni)
-    {
-    }
+    
+ public Estudiante(int claveIni, String nombreIni)
+ {
+        nombre = nombreIni;
+        clave = claveIni;
+  }
+ /**
+ * muestra los detalles clave y nombre de un alumno
+ 
+ */
     public String dimeDetalles()
     {
         return "Clave:" + clave + "Nombre:" + nombre;
     }
+ /**
+ * retorna la clave la cual puede usarse en otras clases
+ 
+ */
+ public int dimeClave()
+ {
+        return clave;
+  }
     
 }
--- Grupo.java
+++ Grupo.java
@@ -8,18 +8,72 @@ public class Grupo
     estudiantes = new Estudiante[totalEstudiantes];
     this.nomMateria = nomMateria;
 }
-public void inscribir(Estudiante unEstudiante)
+/**
+ * da de baja a un alumno
+ * regresa verdadero o falso si se pudo o no dar de baja
+ */
+public boolean darDeBaja(int claveBaja)
+{
+    int i;
+    for(i=0; i<estudiantes.length;i++)
+    if(estudiantes[i].dimeClave() == claveBaja)
     {
-        int i;
-        for(i=0;i<estudiantes.length; i++){
-            if(estudiantes[i] == null){
-            estudiantes[i] = unEstudiante;
-        }
+        estudiantes[i]=null;
+        return true;
     }
+    return false;
+    
+}
+/**
+ * busca un espacio disp
+ * regresa la poc nula en el arr en caso contrario regresa -1
+ */
+private int buscaEspacioDisponible()
+{
+    int i;
+    for(i=0;i<estudiantes.length;i++){
+        if (estudiantes[1] == null){
+            return i;
+        }
         
     }
-public void darDeBaja(int claveEstudiante)
-{
+    return -1;
     
+}
+/**
+ * inscreibe un estudiante
+ * regresa verdadero o falso si se pudo o no inscribir
+ */
+public boolean inscribir(Estudiante unEstudiante)
+{
+    int existe = this.buscarEstudiante(unEstudiante.dimeClave());
+    if(existe != -1){
+        return false;
+    }
+    int posDisponible = this.buscaEspacioDisponible();
+
+   if(posDisponible == -1){
+       return false;
+    }
+    estudiantes[posDisponible] = unEstudiante;
+    return true;
+    
+    
+     
+    
+}
+/**
+ * busca un estudiante por medio de su clave
+ * y lo regresa el num. en el que se encuentra, si no lo encuentra regresa -1
+ */
+public int buscarEstudiante(int claveEstudiante)
+{
+    for(int i=0;i<estudiantes.length; i++)
+    {
+        if(estudiantes[i].dimeClave() == claveEstudiante){
+            return i;
+        }
+    }
+    return -1;
 }
 }
